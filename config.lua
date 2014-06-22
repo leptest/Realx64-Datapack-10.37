cf1fa68661adb34d71769e7e813fb11734d0a8ad
Daniel Chapman <leptest@gmail.com>
2014-06-22 21:53:05 +0100

    Updated basic server config and exp stages

--- config.lua
+++ config.lua
@@ -1,76 +1,94 @@
 -- Combat settings
-worldType = "pvp"
+-- NOTE: valid values for worldType are: "pvp", "no-pvp" and "pvp-enforced"
+worldType = "pvp-enforced"
 hotkeyAimbotEnabled = "yes"
-protectionLevel = 50
-killsToRedSkull = 6
-killsToBlackSkull = 10
-pzLocked = 30000
-removeAmmoWhenUsingDistanceWeapon = "yes"
+protectionLevel = 200
+killsToRedSkull = 3
+killsToBlackSkull = 6
+pzLocked = 60000
+removeAmmoWhenUsingDistanceWeapon = "no"
 removeChargesFromRunes = "yes"
-timeToDecreaseFrags = 3 * 60 * 60 * 1000
-whiteSkullTime = 10 * 60 * 1000
+-- \/ = 1 day
+timeToDecreaseFrags = 24 * 60 * 60 * 1000
+whiteSkullTime = 15 * 60 * 1000
 stairJumpExhaustion = 1000
-experienceByKillingPlayers = "no"
+experienceByKillingPlayers = "yes"
+expFromPlayersLevelRange = 150
+noDamageToSameLookfeet = "no"
 
 -- Connection Config
-ip = "127.0.0.1"
+-- NOTE: maxPlayers set to 0 means no limit
+passwordType = "sha1"
+ip = "powerdungeon.no-ip.org"
 bindOnlyGlobalAddress = "no"
 loginProtocolPort = 7171
 gameProtocolPort = 7172
 statusProtocolPort = 7171
-maxPlayers = "1000"
-motd = "Welcome to The Forgotten Server!"
-onePlayerOnlinePerAccount = "yes"
+-- when this is 0, it waits in que all the time
+maxPlayers = 1000
+motd = "Brrrrraaaaaapppppppp!"
+onePlayerOnlinePerAccount = "no"
 allowClones = "no"
-serverName = "Forgotten"
-statusTimeout = 60000
+serverName = "PowerDungeon 10.37"
+statusTimeout = 5000
 replaceKickOnLogin = "yes"
-maxPacketsPerSecond = 2500
+maxPacketsPerSecond = 25
+
+
+-- Starting position
+--newPlayerTownId = 1
+newPlayerSpawnPosX = 32369
+newPlayerSpawnPosY = 32241
+newPlayerSpawnPosZ = 7
 
 -- Deaths
+-- NOTE: Leave deathLosePercent as -1 if you want to use the default
+-- death penalty formula. For the old formula, set it to 10. For
+-- no skill/experience loss, set it to 0.
 deathLosePercent = -1
 
 -- Houses
-housePriceEachSQM = 5000
+-- NOTE: set housePriceEachSQM to -1 to disable the ingame buy house functionality
+housePriceEachSQM = 1000
 houseRentPeriod = "never"
 
 -- Item Usage
-timeBetweenActions = 1000
+timeBetweenActions = 200
 timeBetweenExActions = 1000
 
 -- Map
+-- NOTE: set mapName WITHOUT .otbm at the end
 mapName = "realmap"
-mapAuthor = "DarkShaoz"
+mapAuthor = "Komic"
 
 -- Market
 marketOfferDuration = 30 * 24 * 60 * 60
-premiumToCreateMarketOffer = "no"
+premiumToCreateMarketOffer = "yes"
 checkExpiredMarketOffersEachMinutes = 60
 maxMarketOffersAtATimePerPlayer = 100
 
 -- MySQL
 mysqlHost = "127.0.0.1"
-mysqlUser = "root"
-mysqlPass = "pass"
-mysqlDatabase = "mydb"
+mysqlUser = "otserver"
+mysqlPass = "BcfyEqUMqPquw5wR"
+mysqlDatabase = "otserver"
 mysqlPort = 3306
 mysqlSock = ""
-passwordType = "sha1"
 
 -- Misc.
 allowChangeOutfit = "yes"
 freePremium = "yes"
 kickIdlePlayerAfterMinutes = 15
-maxMessageBuffer = 4
-noDamageToSameLookfeet = "no"
-emoteSpells = "no"
+maxMessageBuffer = 5
+emoteSpells = "yes"
 
 -- Rates
-rateExp = 20
-rateSkill = 20
-rateLoot = 5
-rateMagic = 10
-rateSpawn = 5
+-- NOTE: rateExp is not used if you have enabled stages in data/XML/stages.xml
+rateExp = 999
+rateSkill = 30
+rateLoot = 4
+rateMagic = 30
+rateSpawn = 2
 
 -- Monsters
 deSpawnRange = 2
@@ -80,11 +98,12 @@ deSpawnRadius = 50
 staminaSystem = "yes"
 
 -- Startup
+-- NOTE: defaultPriority only works on Windows and sets process priority.
 defaultPriority = "high"
 startupDatabaseOptimization = "no"
 
 -- Status server information
-ownerName = "Dark Shaooz"
-ownerEmail = "darkshaoz@hotmail.com"
-url = "http://otland.net/members/dark-shaooz.38177/"
-location = "USA"
+ownerName = "AC DC"
+ownerEmail = ""
+url = "http://powerdungeon.no-ip.org/"
+location = "United Kingdom"
